cfefd232ccbf23bc383973b7062a19015b160a81
Zheltov Stepan <Zheltov@mail.ru>
2016-09-16 12:20:43 +0400

    POEHALI 4 Please enter the commit message for your changes. Lines starting y Changes to be committed:

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n,n1,sum=0;
+    cout<<"Vvedite chisla n,n1"<< endl;
+    cin>>n;
+    cin>>n1;
+    sum=n+n1;
     return 0;
 }
