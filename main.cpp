df3ba137cefeef4e4886aa61f0f4006a24c39baf
Zheltov Stepan <Zheltov@mail.ru>
2016-09-16 13:01:15 +0400

    POEHALI 5

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
-    int n,n1,sum=0;
+    int n3,n,n1,sum=0;
     cout<<"Vvedite chisla n,n1"<< endl;
     cin>>n;
     cin>>n1;
     sum=n+n1;
+    n3=n-n1;
     return 0;
+
 }
